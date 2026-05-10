fb92d90a431795ca8dad204dd6040afa4d90c6ca
Flaysar <egorhohlov999@mail.ru>
2026-05-10 21:17:22 +0300

    change configs and env

--- backend/db.py
+++ backend/db.py
@@ -37,8 +37,17 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+def build_db_url():
+    return (
+        f"postgresql://{os.getenv('POSTGRES_USER')}:"
+        f"{os.getenv('POSTGRES_PASSWORD')}@"
+        f"{os.getenv('POSTGRES_HOST')}:"
+        f"{os.getenv('POSTGRES_PORT')}/"
+        f"{os.getenv('POSTGRES_DB')}"
+    )
+
 def get_connection():
-    dsn = os.getenv("DATABASE_URL")
+    dsn = build_db_url()
 
     logger.info(f"[DB] Connecting with DSN: {dsn}")
 
